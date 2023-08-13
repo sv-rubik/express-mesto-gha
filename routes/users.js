9336e7b4822781053eae52680b4b980ed7d4518d
sv-rubik <tosvr@yandex.ru>
2023-08-13 18:34:23 +0300

    fix: user routes order changed

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,8 @@ const {
 // Поэтому, когда внутри users.js определяем маршруты,
 // начинающиеся с /users, мы уже находимся в контексте этого подпути и в path не нужен '/users'
 userRouter.get('/', getUserList); // GET запрос будет обращаться к http://localhost:3000/users
+userRouter.get('/me', getUserProfile); // должен стоять до роута '/:userId', т.к. иначе me воспринимается, как id
 userRouter.get('/:userId', validateUserID, getUserByID); // GET запрос будет обращаться к http://localhost:3000/users/1
-userRouter.get('/me', getUserProfile);
 // userRouter.post('/', createUser); // POST запрос будет обращаться к http://localhost:3000/users
 userRouter.patch('/me', validateUserProfile, updateUserProfile);
 userRouter.patch('/me/avatar', validateUserAvatar, updateUserAvatar);
