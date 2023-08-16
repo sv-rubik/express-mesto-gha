fcfba351f17ee984cb561f03b04bede17788e9bc
sv-rubik <tosvr@yandex.ru>
2023-08-16 21:12:05 +0300

    feat: server protection added

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const { errors } = require('celebrate');
 
 const { PORT = 3000, BASE_PATH = 'localhost' } = process.env;
 
+// защита приложения
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
 const errorsHandler = require('./middlewares/errorsHandler');
 const userRouter = require('./routes/users');
@@ -15,6 +26,9 @@ const {
   validateCreateUser,
   validateLogin,
 } = require('./middlewares/celebrateValidation');
+
+app.use(helmet());
+app.use(limiter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
